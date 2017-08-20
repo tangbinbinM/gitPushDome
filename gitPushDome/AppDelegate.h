6f75d695f04d1cd0db61b18eae22b6c69333019e
YiGuo <>
2017-08-20 10:33:27 +0800

    这里写注释

---
+++ gitPushDome/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  gitPushDome
+//
+//  Created by YiGuo on 2017/8/20.
+//  Copyright © 2017年 tbb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ gitPushDome/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  gitPushDome
+//
+//  Created by YiGuo on 2017/8/20.
+//  Copyright © 2017年 tbb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
